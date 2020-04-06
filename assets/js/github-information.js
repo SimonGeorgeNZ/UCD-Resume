7117b3562ecd261f31607cc05e80488d739ec7e3
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2020-04-06 13:10:34 +0000

    fixed bugs to git hub page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -32,11 +32,13 @@ function repoInformationHTML(repos) {
                 </p>
                 <ul>
                     ${listItemsHTML.join("\n")}
-                </ul>
+                </ul> 
             </div>`
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
